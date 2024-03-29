e17a430b0c396c29670db9d8959bd756a3863d66
wxl <297750182@qq.com>
2024-03-29 14:26:57 +0800

    :truck: 包名及路径调整

--- integer/long.go
+++ integer/long.go
@@ -1,7 +1,8 @@
-package str
+package integer
 
 import (
 	"bytes"
+	kb "github.com/wxlbd/kit/bytes"
 	"strconv"
 )
 
@@ -10,7 +11,7 @@ type Long int64
 
 func (I *Long) UnmarshalJSON(b []byte) error {
 	b = bytes.Trim(b, "\"")
-	parseInt, err := strconv.ParseInt(BytesToString(b), 10, 64)
+	parseInt, err := strconv.ParseInt(kb.BytesToString(b), 10, 64)
 	if err != nil {
 		return err
 	}
@@ -20,5 +21,5 @@ func (I *Long) UnmarshalJSON(b []byte) error {
 
 func (I Long) MarshalJSON() ([]byte, error) {
 	str := `"` + strconv.FormatInt(int64(I), 10) + `"`
-	return StringToBytes(str), nil
+	return kb.StringToBytes(str), nil
 }
